6aee8cbabbc1f00d27792a7f0d8322459022b7a3
Thabani Tembe <thabanitembe@hotmail.com>
2018-07-05 16:02:59 +0200

    got 1st test passing

---
+++ Depreciation-Kata/StringCalculator.Tests/DepreciationKataTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class DepreciationKataTests
+    {
+        [TestCase(100,100, 0, 1)]
+        [TestCase(18, 100, 10, 5)]
+        [TestCase(1000, 2500, 500, 2)]
+        public void Calculate_GivenValidAssetCostWithSalvageValueAndAssetOwnedForEntireYear_ShouldReturnDepreciationAmount(double expected, double assetCost, int salvageValue, int usefulLife) {
+            //Arrange
+            var acquiredDate = new DateTime(2017, 01, 01);
+            var financialYearEndDate = new DateTime(2017, 12, 31);
+            var sut = new DepreciationKata();
+         
+            //Act
+            var actual = sut.CalculationDepreciationAmount(assetCost ,salvageValue, usefulLife, acquiredDate, financialYearEndDate);
+            //Assert
+
+            Assert.AreEqual(expected,actual);
+        }
+
+
+        //[TestCase(50, 100, 0, 1)]
+        //public void Calculate_GivenValidAssetCostWithSalvageValueAndAssetOwnedForHalfAYear_ShouldReturnDepreciationAmount(double expected, double assetCost, int salvageValue, int usefulLife)
+        //{
+        //    //Arrange
+        //    var acquiredDate = new DateTime(2016, 07, 01);
+        //    var financialYearEndDate = new DateTime(2017, 06, 30);
+        //    var sut = new DepreciationKata();
+
+        //    //Act
+        //    var actual = sut.CalculationDepreciationAmount(assetCost, salvageValue, usefulLife, acquiredDate, financialYearEndDate);
+        //    //Assert
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+    }
+}
+
---
+++ Depreciation-Kata/StringCalculator/DepreciationKata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class DepreciationKata
+    {
+        public object CalculationDepreciationAmount(double assetCost, double salvageValue, int usefulLife, DateTime acquiredDate, DateTime financialYearEndDate)
+        {
+            var yearDepreciationAmounts = new Dictionary<int, double>();
+            var life = usefulLife;
+            int acquiredDay = acquiredDate.Day;
+            int acquiredYear = acquiredDate.Year;
+            var depreciationAmount = 0.00; 
+            if (acquiredDay > 1)
+            {
+                acquiredDay = 1;
+            }
+            while (life >=1) {
+                depreciationAmount = (assetCost - salvageValue) / usefulLife;
+                yearDepreciationAmounts.Add(acquiredYear, depreciationAmount);
+                assetCost -= depreciationAmount;
+                acquiredYear++;
+                life--;
+            }            
+                depreciationAmount = yearDepreciationAmounts[financialYearEndDate.Year];
+
+              return depreciationAmount;
+        }
+    }
+}
